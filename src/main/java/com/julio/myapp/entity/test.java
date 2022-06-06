c60d2ce9832cac21e8d27180944046a8d69f48bf
julioAzocar <julio.azocar@hotmail.com>
2022-06-06 14:58:36 -0300

    Nombre proyecto: my_app, descripcion: version final proyecto maven

---
+++ src/main/java/com/julio/myapp/entity/test.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package com.julio.myapp.entity;
+
+/**
+ * @author julio
+ *
+ */
+public class test {
+	private String nombres;
+
+	/**
+	 * @return the nombres
+	 */
+	public String getNombres() {
+		return nombres;
+	}
+
+	/**
+	 * @param nombres the nombres to set
+	 */
+	public void setNombres(String nombres) {
+		this.nombres = nombres;
+	}
+
+
+}
